3387ea82a1bcf062f52a4094e9fd4b4280a2388e
techlearn-center <144989969+techlearn-center@users.noreply.github.com>
2024-01-20 21:24:19 -0500

    Update web.sh

--- web.sh
+++ web.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-yum install wget unzip httpd -y
-systemctl start httpd
-systemctl enable httpd
-wget https://www.tooplate.com/zip-templates/2117_infinite_loop.zip
-unzip -o 2117_infinite_loop.zip
-cp -r 2117_infinite_loop/* /var/www/html/
-systemctl restart httpd
+
+sudo yum install wget unzip httpd -y
+sudo systemctl start httpd
+sudo systemctl enable httpd
+sudo wget https://www.tooplate.com/zip-templates/2117_infinite_loop.zip
+sudo unzip -o 2117_infinite_loop.zip
+sudo cp -r 2117_infinite_loop/* /var/www/html/
+sudo systemctl restart httpd
