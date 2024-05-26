83ad972210489bf8ad7f25274259a56dd42ed4a7
techlearn-center <144989969+techlearn-center@users.noreply.github.com>
2024-05-26 19:54:33 -0400

    Create aws-s3.py

---
+++ aws-s3.py
@@ -0,0 +1,26 @@
+import boto3
+
+
+# Connect to AWS S3 through Boto3
+s3 = boto3.client("s3")
+
+#Specify a valid AWS region
+
+region = 'us-east-1'
+
+BUCKETNAME = "techlearnboto3"
+
+# Create S3 Bucket with region constraint
+bucketResponse =s3.create_bucket(
+        Bucket=BUCKETNAME
+)
+
+# Get the bucket name from the response (optional, not needed for upload/download)
+#BUCKETNAME = "techlearnboto3"
+
+#Upload Files to the S3 bucket
+filePath = "/opt/"
+fileToUpload = "2136_kool_form_pack.zip"
+fileToS3 = filePath + fileToUpload
+s3.upload_file(fileToS3, BUCKETNAME, fileToUpload)
+print(f"S3 bucket '{BUCKETNAME}' created successfully in region '{region}'")
